692aca12f0f1b55fc7c7281754a4df55a1196d14
Musa Rafik <musa_rafik@utexas.edu>
2020-03-01 19:34:16 -0600

    added landing page video and starting on navbar

---
+++ frontend/src/Landing.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './Landing.css';
+import ReactPlayer from 'react-player';
+import video from './assets/rocket_landing_page_video.mp4';
+import NavBar from './NavBar.js';
+
+
+class Landing extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render(){
+    return(
+      <div className="Landing">
+        <ReactPlayer className="video" url={video} playing loop></ReactPlayer>
+        <NavBar />
+      </div>
+    );
+  }
+}
+
+export default Landing; 
---
+++ frontend/src/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './NavBar.css';
+
+class NavBar extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    render(){
+        return(
+            <a href="test">test</a>
+        )
+    }
+}
+
+export default NavBar; 
