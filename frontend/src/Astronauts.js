bf71bf193dacbe6a6d682ff2793817219eb03d62
Shawn Victor <ShawnFVictor@gmail.com>
2020-03-05 00:29:59 -0600

    Functional Table in Astronaut Page.

--- frontend/src/Astronauts.js
+++ frontend/src/Astronauts.js
@@ -1,22 +1,60 @@
 import React from 'react';
 import NavBar from './NavBar.js';
+import ReactTable from "react-table-6"
+import "react-table-6/react-table.css"
+
 
 class Astronauts extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-
+          posts: []
         }
     }
 
+    componentDidMount(){
+      const url = "https://jsonplaceholder.typicode.com/posts";
+      fetch(url, {
+        method: "GET"
+      }).then(response => response.json()).then(posts => {
+        this.setState({posts: posts})
+      })
+    }
+
     render(){
+      const columns = [
+        {
+          Header: "Full Name",
+          accessor: "userId"
+        },
+        {
+          Header: "Company",
+          accessor: "id"
+        },
+        {
+          Header: "Country",
+          accessor: "title"
+        },
+        {
+          Header: "Status",
+          accessor: "body"
+        }
+      ]
         return(
-            <div>
-                <NavBar />
-                <h1>Astronauts</h1>
-            </div>
+          <div>
+              <NavBar />
+              <h1>Astronauts</h1>
+              <ReactTable
+                columns={columns}
+              >
+
+
+              </ReactTable>
+          </div>
+
+
         )
     }
 }
 
-export default Astronauts; 
+export default Astronauts;
