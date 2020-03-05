99c84cff0822239d1a84a8786bcaf0cc9fda98d6
Musa Rafik <musa_rafik@utexas.edu>
2020-03-04 21:58:25 -0600

    added links and video for education page

--- frontend/src/Education.js
+++ frontend/src/Education.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NavBar from './NavBar.js';
+import ReactPlayer from 'react-player'; 
 
 class Education extends React.Component{
     constructor(props){
@@ -13,7 +14,12 @@ class Education extends React.Component{
         return(
             <div>
                 <NavBar />
-                <h1>Education</h1>
+                <p>
+                    <a href="https://www.nasa.gov/audience/forstudents/5-8/features/nasa-knows/vehicles/index.html">Information from NASA about rockets.</a><br/>
+                    <a href="https://www.kennedyspacecenter.com/launches-and-events">See information about upcoming launches.</a><br/>
+                    <a href="https://www.nationalgeographic.com/science/space/reference/rockets-and-rocket-launches-explained/">Check out this National Geographic article.</a>
+                </p>
+                <ReactPlayer url="https://www.youtube.com/watch?v=1yBwWLunlOM"/>
             </div>
         )
     }
