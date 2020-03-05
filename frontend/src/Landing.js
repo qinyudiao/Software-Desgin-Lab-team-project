1e4f82e7d8387e0836b7d74f46831b74f5a713a9
Musa Rafik <musa_rafik@utexas.edu>
2020-03-04 21:16:56 -0600

    changed video to carousel for landing page

--- frontend/src/Landing.js
+++ frontend/src/Landing.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Landing.css';
-import ReactPlayer from 'react-player';
-import video from './assets/rocket_landing_page_video.mp4';
 import NavBar from './NavBar.js';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
 
 
 class Landing extends React.Component{
@@ -16,8 +16,19 @@ class Landing extends React.Component{
   render(){
     return(
       <div className="landing">
-        <ReactPlayer className="video" url={video} playing preload loop></ReactPlayer>
         <NavBar className="landingContent" />
+        <Carousel autoPlay showThumbs={false}>
+          <div>
+            <img src='https://i.insider.com/5d0d02fde3ecba05703614b3?width=1600&format=jpeg&auto=webp' />
+          </div>
+          <div>
+            <img src='https://i.insider.com/5d0d02fde3ecba05703614b3?width=1600&format=jpeg&auto=webp' />
+          </div>
+          <div>
+            <img src='https://i.insider.com/5d0d02fde3ecba05703614b3?width=1600&format=jpeg&auto=webp' />
+          </div>
+        </Carousel>
+        
       </div>
     );
   }
