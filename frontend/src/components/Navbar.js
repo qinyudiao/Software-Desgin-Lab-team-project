a78869a91b141c5e97437879439aa38fb084d955
Musa Rafik <musa_rafik@utexas.edu>
2020-03-06 09:18:59 -0600

    small update teo navbar

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Navbar.css';
-import {Link} from 'react-router-dom';
 
 function Navbar(){
     
@@ -62,8 +61,3 @@ function Navbar(){
 }
 
 export default Navbar;
-
-
-            //         <li className="nav-item">
-            //     <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            // </li>
