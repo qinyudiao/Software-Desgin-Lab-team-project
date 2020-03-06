ccc3371c2d8f5c90a48cbe31922550489adb6c7d
LBest42 <lucasbest@utexas.edu>
2020-03-06 11:49:17 -0600

    Add basic information to the About page

--- frontend/src/About.js
+++ frontend/src/About.js
@@ -12,10 +12,17 @@ class About extends React.Component{
 
     render(){
         return(
-            <div>
+            <React.Fragment>
                 <Header />
                 <h1>About</h1>
-            </div>
+                <p> EveryRocketLaunch is a comprehensive database of rocket launches designed to give you as much relevant
+                information as possible about each and every rocket launch. </p>
+                <p> Due to the diverse information available for each rocket launch, this website is designed so that the user can
+                easily navigate to whichever sub-section of information regarding rocket launches they want to look at. </p>
+                <p> Lastly, EveryRocketLaunch is still in development, and our team welcomes any feedback regarding how to make 
+                your experience using our site better! </p>
+                <h3>GitHub Statistics</h3>
+            </React.Fragment>
         )
     }
 }
