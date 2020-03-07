4a2315655eda1a551168780589965aedddd446af
qinyudiao <qdiao@utexas.edu>
2020-03-06 18:35:43 -0600

    api update

--- backend/app.js
+++ backend/app.js
@@ -12,10 +12,20 @@ app.get('/', (req, res) => {
     res.send("Hello World!");
 });
 
-app.get('/launches', async (request, response) => {
+app.get('/launch', async (request, response) => {
   console.log(request.params);
 
-  const launches_url = `https://launchlibrary.net/1.4/launch?startdate=2015-02-20&enddate=2015-09-20`;
+  const launches_url = `https://launchlibrary.net/1.4/launch?limit=10000`;
+  const launches_response = await fetch(launches_url);
+  const launches_data = await launches_response.json();
+
+  response.json(launches_data);
+});
+
+app.get('/launch/next/5', async (request, response) => {
+  console.log(request.params);
+
+  const launches_url = `https://launchlibrary.net/1.4/launch/next/5`;
   const launches_response = await fetch(launches_url);
   const launches_data = await launches_response.json();
 
