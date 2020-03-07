4923778252d213bbca8a2a6d092f9c7b3220a680
qinyudiao <qdiao@utexas.edu>
2020-03-06 18:00:05 -0600

    api request successful

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,26 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const fetch = require('node-fetch');
 var cors = require('cors');
 var country = require('./routes/country.js');
 
 app.use('/country', country); 
 app.use(cors());
 
-app.listen(4000, function(){
-    console.log('Example app listening on port 4000!');
-});
 
 app.get('/', (req, res) => {
     res.send("Hello World!");
 });
+
+app.get('/launches', async (request, response) => {
+  console.log(request.params);
+
+  const launches_url = `https://launchlibrary.net/1.4/launch?startdate=2015-02-20&enddate=2015-09-20`;
+  const launches_response = await fetch(launches_url);
+  const launches_data = await launches_response.json();
+
+  response.json(launches_data);
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
