13ca8db0de0d9cf9305ca4ef7e0f089f61536f55
Musa Rafik <musa_rafik@utexas.edu>
2020-03-25 15:26:54 -0500

    got next launch from api, just need to render on frontend

---
+++ backend/routes/landingpage.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+router.get('/', (req, res) =>{
+    request('https://fdo.rocketlaunch.live/json/launches/next/5', (error, response, body) =>{
+        if(!error && response.statusCode == 200){
+            res.send(getLastLaunch(JSON.parse(response.body)));
+        }
+        else{
+            console.log(error);
+        }
+    });
+})
+
+// In the JSON response, the next launch that will occur is the first launch
+getLastLaunch = (recentLaunches) =>{
+    let launch_descriiption = recentLaunches['result'][0]['launch_description'];
+    let launch = {'launchDescription': launch_descriiption}
+    return launch;
+}
+
+module.exports = router;
--- frontend/src/Landing.js
+++ frontend/src/Landing.js
@@ -11,8 +11,28 @@ class Landing extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-
+      recentLaunch: null
     }
+  }
+
+  getRecentLaunch = () =>{
+    let url = '';
+    if(process.env.NODE_ENV === 'production'){
+      url = 'http://ec2-54-226-123-223.compute-1.amazonaws.com/landing';
+    }
+    else{
+      url = '/landing';
+    }
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      this.setState({recentLaunch:data})
+    })
+  }
+
+  componentDidMount(){
+    this.getRecentLaunch();
   }
 
   render(){
