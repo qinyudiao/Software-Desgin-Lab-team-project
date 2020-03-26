da43d5049967144b6cce20b4d7965a8e7c46ae3c
qinyudiao <qdiao@utexas.edu>
2020-03-26 16:36:46 -0500

    fixed path

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import './App.css';
+import './css/App.css';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Landing from './Landing.js';
 import About from './About.js';
