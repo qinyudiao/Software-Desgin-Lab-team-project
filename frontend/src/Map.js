8e57aed1d28a7c59c0984ed83de6ecd02a6dd1a9
qinyudiao <qdiao@utexas.edu>
2020-03-27 23:51:43 -0500

    add map basic layout and real time total launches

--- frontend/src/Map.js
+++ frontend/src/Map.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import './css/Map.css';
+import moment from 'moment';
+import 'moment-timezone';
 import Header from './Header';
+import MapContainer from './components/MapContainer.js';
  
-const AnyReactComponent = ({ text }) => <div>{text}</div>;
-const handleApiLoaded = (map, maps) => {
-  // use map and maps objects
-};
+// const AnyReactComponent = ({ text }) => <div>{text}</div>;
+// const handleApiLoaded = (map, maps) => {
+//   // use map and maps objects
+// };
+
 
 class Map extends Component {
   static defaultProps = {
@@ -14,17 +19,57 @@ class Map extends Component {
     },
     zoom: 15
   };
- 
 
-    render(){
-        return(
-            <React.Fragment style={{ height: '100vh', width: '100%' }}>
-              <Header></Header>
-
-
-            </React.Fragment>
-        )
+  constructor(props){
+    super(props);
+    this.state = {
+      launch_number: ""
     }
+}
+
+  componentDidMount(){
+    const date = moment().format("YYYY-MM-DD");
+    //console.log(date);
+    const url = `https://launchlibrary.net/1.4/launch?limit=5000?startdate=1960-08-20&enddate=${date}`;
+    console.log();
+    fetch(url, {
+        method: "GET"
+    })
+    .then(response => response.json())
+    .then(data => {
+        this.setState({launch_number: data.count})
+        //console.log(data.count);
+    });
+  }
+
+  render(){
+      return(
+          <React.Fragment>
+            <Header></Header>
+            <div className="black-board">
+              <span id="left-column" className="column">
+                <div id="total-launches" className="column-item">
+                  <h6>Total Launches</h6>
+                  <p id="total-launches-number"> {this.state.launch_number}</p>
+                </div>
+                <div id="launches-by-region" className="column-item">
+                  <h3>Launches by Region</h3>
+                </div>
+              </span>
+              <span id="middle-column" className="column">
+                <div id="map-container">
+                  <MapContainer />
+                </div>
+              </span>
+              <span id="right-column" className="column">
+                <div id="launches-plot-year" className="column-item">
+                  <p>Plot: Launches per year</p>
+                </div>
+              </span>
+            </div>
+          </React.Fragment>
+      )
+  }
 }
 
 export default Map; 
