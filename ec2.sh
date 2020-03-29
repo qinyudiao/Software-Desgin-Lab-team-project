79ec50416a9be7b973825ca0c02b4d5d8d2d2852
Musa Rafik <31826652+musarafik@users.noreply.github.com>
2020-03-29 11:24:12 -0500

    Removed shell script to access ec2

--- ec2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
- cd ~/Documents/Academic/Spring2020/461L/Software-Desgin-Lab-team-project/backend
-ssh -i "everyrocketlaunch.com-key.pem" ec2-user@ec2-54-226-123-223.compute-1.amazonaws.com
-
