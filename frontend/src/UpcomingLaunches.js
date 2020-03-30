5152e4fb8ad6c40f24984b6e3f06b3406e0bc96a
qinyudiao <qdiao@utexas.edu>
2020-03-30 16:41:22 -0500

    add countdown on upcoming 1

--- frontend/src/UpcomingLaunches.js
+++ frontend/src/UpcomingLaunches.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react'
 import './css/UpcomingLaunches.css';
 import Header from './components/Header';
+import moment from 'moment';
+import 'moment-timezone';
+
+// function getTimeCountDown(launches) {
+//     let launchTime;
+//     launches.forEach((launch) => {
+//         launchTime = new Date(Number(launch.isonet.slice(0,4)), Number(launch.isonet.slice(4,6)), Number(launch.isonet.slice(6,8)),  Number(launch.isonet.slice(9,11)), Number(launch.isonet.slice(11,13)), Number(launch.isonet.slice(13,15)));
+//         launchTimes = [...LaunchTimes, launchTime];
+//     })
+//     return launchTime;
+// }
 
 export default class UpcomingLaunches extends Component {
     constructor(props){
         super(props);
         this.state = {
-            launches: []
+            launches: [],
+            countdowns: []
         }
     }
 
@@ -18,14 +30,32 @@ export default class UpcomingLaunches extends Component {
         })
         .then(response => response.json())
         .then(data => {
+        //     let curTime = moment().utc().format('YYYYMMDDThhmmss');
+        //     curTime = new Date(Number(curTime.slice(0,4)), Number(curTime.slice(4,6)), Number(curTime.slice(6,8)),  Number(curTime.slice(9,11)), Number(curTime.slice(11,13)), Number(curTime.slice(13,15)));
+        //     console.log(curTime);
+            
+
+        //    // var diff = date2 - date1;
+        //     // var diff = new moment.duration(diff);
+        //     // console.log(diff.asDays());
+        //     // console.log(diff.asHours());
+        //     // console.log(diff.asMinutes());
+        //     // console.log(diff.asSeconds());
+        //     console.log(data.launches[0].isonet);
+        //     console.log(curTime);
             this.setState({
                 launches: data.launches,
+                // countdowns: getTimeCountDown(data.launches),
             });
         });
     };
 
+    componentDidUpdate(){
+
+    }
+
     render() {
-        const b = this.state.launches.map((launch, index) => <div  key={index} className="upcoming-item">
+        const launches = this.state.launches.map((launch, index) => <div  key={index} className="upcoming-item">
             <h2>Launch {index+1}</h2>
             <div className="Launch-box">
                 <span style={{width: "55%"}}>
@@ -35,7 +65,7 @@ export default class UpcomingLaunches extends Component {
                 </span>
                 <span className="info-box" style={{width: "45%"}}>
                     <h3 style={{marginBottom: "3.5%"}}>{launch.name}</h3>
-                    <p style={{marginBottom: "1%"}}>Time till Launch: for phase3</p>
+                    <p style={{marginBottom: "1%"}}>{`Time till Launch: for phase3`}</p>
                     <p style={{marginBottom: "1%"}}>Agency: {launch.lsp.name}</p>
                     <p style={{marginBottom: "1%"}}>Time: {launch.net}</p>
                     <p style={{marginBottom: "1%"}}>Location: {launch.location.name}</p>
@@ -46,7 +76,7 @@ export default class UpcomingLaunches extends Component {
                 </span>
             </div>
         </div>)
-        const a = <h1 className="upcoming-item">something</h1>;
+
         return (
             <React.Fragment>
                 <div width="100vw">
@@ -54,7 +84,7 @@ export default class UpcomingLaunches extends Component {
                     <div className="flex-container">
                         <h1 id="upcoming-title">Upcoming launches</h1>
                         <div className="gallery-container">
-                            {b}
+                            {launches}
                         </div>
                     </div>
                 </div>
