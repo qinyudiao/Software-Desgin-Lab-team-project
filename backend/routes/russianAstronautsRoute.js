ed3cd4ee1a41f2028a46e1f28b588aaa99780ff8
Musa Rafik <musa_rafik@utexas.edu>
2020-04-12 14:27:46 -0500

    converted all russian names to proper casing to amek searching work with wikipedia

--- backend/routes/russianAstronautsRoute.js
+++ backend/routes/russianAstronautsRoute.js
@@ -6,7 +6,7 @@ const cron = require('node-cron');
 let russianAstronaut = require('../models/russianAstronautSchema.js');
 
 // At a periodic time update database with international astronaut information
-cron.schedule('0 13 * * Sunday', () =>{
+cron.schedule('2 * * * * *', () =>{
 // cron.schedule('2 * * * * *', () => {
     console.log('running international astronaut cron job');
     request('https://raw.githubusercontent.com/ShawnVictor/demo/master/db3.json', (err, res) =>{
@@ -21,6 +21,8 @@ cron.schedule('0 13 * * Sunday', () =>{
                         console.log("Russian Astronaut already in database");
                     }
                     else{
+                        console.log("responseas;asfsa", responseArray[i]);
+                        responseArray[i]['A'] = lowerCase(responseArray[i]['A']);
                         russianAstronaut.create(responseArray[i], (err, result) =>{
                             if(err){
                                 console.log(err);
@@ -36,6 +38,15 @@ cron.schedule('0 13 * * Sunday', () =>{
     });
 });
 
+lowerCase = (name) =>{
+    convertedName = "";
+    name = name.split(" ")
+    for(let i = 0; i < name.length; i++){
+        convertedName += name[i].charAt(0).toUpperCase() + name[i].slice(1).toLowerCase() + " ";
+    }
+    return convertedName;
+}
+
 // Query database to get all Russian astronauts then send results to frontend
 router.get('/', (req, res) =>{
     russianAstronaut.find({}, (err, response) =>{
@@ -50,7 +61,41 @@ router.get('/', (req, res) =>{
 });
 
 router.get('/:astronautId/:type', (req, res) =>{
-    console.log("russian WORKS")
+    let nameArray = req.params.astronautId.split(" ");
+
+    // Take out extra punctuation like commas or periods
+    for(let i = 0; i < nameArray.length; i++){
+        nameArray[i] = nameArray[i].replace(",", ""); 
+        nameArray[i] = nameArray[i].replace(".", "");
+    }
+
+    // Take out blank spaces and initials since they mess up formation of full name
+    let finalNameArray = []
+    for(let i = 0; i < nameArray.length; i++){
+        if(nameArray[i] !== '' && nameArray[i].length > 1){
+            finalNameArray.push(nameArray[i]);
+        }
+    }
+
+    // order of names in array is expected to be [middle name, last name, first name]
+    let firstName = finalNameArray[finalNameArray.length - 1];
+    let lastName = finalNameArray[finalNameArray.length - 2];
+    let middleNames = []; // An array for names with multiple middle names
+    if(finalNameArray.length > 2){
+        middleNames = finalNameArray.slice(0, nameArray.length - 2);
+    }
+    let middleName = ' ';
+    for(let i = 0; i < middleNames.length; i++){ // Combine middle names into one string
+        if(middleNames[i].length > 1){
+            middleName += middleNames[i];
+        }
+    }
+    let fullName = firstName + middleName + ' ' + lastName; // Create full name to pass into request
+    let url = "http://en.wikipedia.org/api/rest_v1/page/summary/" + fullName;
+    request(url, (req, response) =>{
+        let results = JSON.parse(response.body);
+        res.send(results);
+    });
 });
 
 module.exports = router;
