54a0d256602b37b25bcb4bf03d3f5101d72782a8
Musa Rafik <musa_rafik@utexas.edu>
2020-04-20 11:30:30 -0500

    basic Google seach works I think

--- backend/app.js
+++ backend/app.js
@@ -44,96 +44,6 @@ app.use('/location', location);
 app.use('/mission', mission);
 app.use('/pad', pad);
 
-let searchResults = [];
-
-doSearch = (searchTermArray) =>{
-    for(let searchTerm in searchTermArray){
-        Launch.find({
-            $text:{
-                $search: searchTerm,
-                $caseSensitive: false
-            }
-        }, (err, res) =>{
-            if(err){
-                console.log(err);
-            }
-            else{
-                searchResults.push(res);
-            }
-        });
-
-        UsAstronaut.find({
-            $text:{
-                $search: searchTerm,
-                $caseSensitive: false
-            }
-        }, (err, res) =>{
-            if(err){
-                console.log(err);
-            }
-            else{
-                searchResults.push(res);
-            }
-        });
-
-        RussianAstronaut.find({
-            $text:{
-                $search: searchTerm,
-                $caseSensitive: false
-            }
-        }, (err, res) =>{
-            if(err){
-                console.log(err);
-            }
-            else{
-                searchResults.push(res);
-            }
-        });
-
-        InternationalAstronaut.find({
-            $text:{
-                $search: searchTerm,
-                $caseSensitive: false
-            }
-        }, (err, res) =>{
-            if(err){
-                console.log(err);
-            }
-            else{
-                searchResults.push(res);
-            }
-        });
-
-        Agency.find({
-            $text:{
-                $search: searchTerm,
-                $caseSensitive: false
-            }
-        }, (err, res) =>{
-            if(err){
-                console.log(err);
-            }
-            else{
-                searchResults.push(res);
-            }
-        });
-    }
-
-}
-
-// Handle Google like search
-app.post('/search', async (req, res) =>{
-    try{
-        let search = req.body.search;
-        let searchTermArray = search.split(' ');
-        await doSearch(searchTermArray);
-        res.send({'response': searchResults});
-    }
-    catch(err){
-        console.log(err);
-    }
-});
-
 // MongoDB stuff
 const uri = 'mongodb+srv://admin:admin@softwarelab-zbga3.mongodb.net/SoftwareLab?retryWrites=true&w=majority';
 mongoose.connect(uri, { useNewUrlParser: true });
@@ -155,12 +65,165 @@ let agencyCollection = db.collection('agencies');
 let internationalAstronautCollection = db.collection('internationalastronauts');
 let russianAstronautCollection = db.collection('russianastronauts');
 let usAstronautCollection = db.collection('usastronauts');
-
 launchCollection.createIndex({"$**": "text"});
 agencyCollection.createIndex({"$**": "text"});
 internationalAstronautCollection.createIndex({"$**": "text"});
 russianAstronautCollection.createIndex({"$**": "text"});
 usAstronautCollection.createIndex({"$**": "text"});
+
+searchLaunches = (searchTerm) =>{
+    return new Promise(async (resolve, reject) =>{
+        try{
+            Launch.find({
+                $text:{
+                    $search: searchTerm,
+                    $caseSensitive: false
+                }
+            }, (err, res) =>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    resolve(res);
+                }
+            });
+        }
+        catch(err){
+            console.log(err);
+        }
+    });
+}
+
+searchInternationalAstronauts = (searchTerm) =>{
+    return new Promise(async (resolve, reject) =>{
+        try{
+            InternationalAstronaut.find({
+                $text:{
+                    $search: searchTerm,
+                    $caseSensitive: false
+                }
+            }, (err, res) =>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    resolve(res);
+                }
+            });
+        }
+        catch(err){
+            console.log(err);
+        }
+    });
+}
+
+searchRussianAstronauts = (searchTerm) =>{
+    return new Promise(async (resolve, reject) =>{
+        try{
+            RussianAstronaut.find({
+                $text:{
+                    $search: searchTerm,
+                    $caseSensitive: false
+                }
+            }, (err, res) =>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    resolve(res);
+                }
+            });
+        }
+        catch(err){
+            console.log(err);
+        }
+    });
+}
+
+searchUsAstronauts = (searchTerm) =>{
+    return new Promise(async (resolve, reject) =>{
+        try{
+            UsAstronaut.find({
+                $text:{
+                    $search: searchTerm,
+                    $caseSensitive: false
+                }
+            }, (err, res) =>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    resolve(res);
+                }
+            });
+        }
+        catch(err){
+            console.log(err);
+        }
+    });
+}
+
+
+searchAgencies = (searchTerm) =>{
+    return new Promise(async (resolve, reject) =>{
+        try{
+            Agency.find({
+                $text:{
+                    $search: searchTerm,
+                    $caseSensitive: false
+                }
+            }, (err, res) =>{
+                if(err){
+                    console.log(err);
+                }
+                else{
+                    resolve(res);
+                }
+            });
+        }
+        catch(err){
+            console.log(err);
+        }
+    });
+}
+
+doSearch = (searchTermArray) =>{
+    return new Promise(async (resolve, reject) => {
+        try{
+            let results = [];
+            for(let i = 0; i < searchTermArray.length; i++){
+                let launchesResult = await searchLaunches(searchTermArray[i]);
+                let agenciesResult = await searchAgencies(searchTermArray[i]);
+                let usAstronautsResult = await searchUsAstronauts(searchTermArray[i]);
+                let russianAstronautsResult = await searchRussianAstronauts(searchTermArray[i]);
+                let internationalAstronautsResult = await searchInternationalAstronauts(searchTermArray[i]);
+                results.push(agenciesResult);
+                results.push(launchesResult);
+                results.push(usAstronautsResult);
+                results.push(russianAstronautsResult);
+                results.push(internationalAstronautsResult);
+            }
+            resolve(results);
+        }
+        catch(err){
+            console.log(err);
+        }
+    });
+}
+
+// Handle Google like search
+app.post('/search', async (req, res) =>{
+    try{
+        let search = req.body.search;
+        let searchTermArray = search.split(' ');
+        let searchResults = [];
+        searchResults = await doSearch(searchTermArray);
+        res.send({'response': searchResults});
+    }
+    catch(err){
+        console.log(err);
+    }
+});
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- backend/routes/launchRoute.js
+++ backend/routes/launchRoute.js
@@ -8,68 +8,68 @@ let Agency = require('../models/agencySchema.js');
 let Rocket = require('../models/rocketSchema.js');
 let Location = require('../models/locationSchema.js');
 
-cron.schedule('22 * 10 * * 1', () =>{
-// cron.schedule('5 * * * * *', () =>{
-    console.log('running launches cron job');
-    for (let i = 0; i<2600; i++) {
-    // for(let i = 0; i < 200; i++){
-    // for(let i = 200; i < 400; i++){
-    // for(let i = 400; i < 600; i++){
-    // for(let i = 600; i < 800; i++){
-    // for(let i = 800; i < 1000; i++){
-    // for(let i = 1000; i < 1200; i++){
-    // for(let i = 1200; i < 1400; i++){
-    // for(let i = 1400; i < 1600; i++){
-    // for(let i = 1600; i < 1800; i++){
-    // for(let i = 1800; i < 2000; i++){
-    // for(let i = 2000; i < 2200; i++){
-        request('https://launchlibrary.net/1.4/launch/' + i, (err, res) =>{
-            if(!err && res.statusCode === 200){
-                let responseObject = JSON.parse(res.body);
-                launchesArray = responseObject.launches;
-                launchesArray.forEach(launch => {
-                    Launch.findOne({id: launch.id, changed: launch.changed}, (error, document) =>{
-                        if(error){
-                            console.log(error);
-                        }
-                        if(document){
-                            // console.log("launches already in database");
-                        }
-                        else{
-                            console.log("new document", launch.id);
-                            Location.findOne({ id: launch.location.id }, (err, foundLocation) => {
-                                // console.log('found location');
-                            })
-                            .then((foundLocation) => {
-                                launch.location = foundLocation;
-                                Agency.findOne({ id: launch.lsp.id }, (err, foundAgency) => {
-                                    // console.log('found agency');
-                                })
-                                .then((foundAgency) => {
-                                    launch.lsp = foundAgency;
-                                    Rocket.findOne({ id: launch.rocket.id }, (err, foundRocket) => {
-                                        // console.log('found rocket');
-                                    })
-                                    .then((foundRocket) => {
-                                        launch.rocket = foundRocket;
-                                        Launch.create(launch, (err) => {
-                                            if(err) {
-                                                console.log(err);
-                                            }
-                                            else {
-                                                console.log(`Launch ${launch.id} saved to database`);
-                                            }
-                                        });
-                                    })
-                                })
-                            })
-                        }
-                    });
-                })
-            }
-        });
-    }
-});
+// cron.schedule('22 * 10 * * 1', () =>{
+// // cron.schedule('5 * * * * *', () =>{
+//     console.log('running launches cron job');
+//     for (let i = 0; i<2600; i++) {
+//     // for(let i = 0; i < 200; i++){
+//     // for(let i = 200; i < 400; i++){
+//     // for(let i = 400; i < 600; i++){
+//     // for(let i = 600; i < 800; i++){
+//     // for(let i = 800; i < 1000; i++){
+//     // for(let i = 1000; i < 1200; i++){
+//     // for(let i = 1200; i < 1400; i++){
+//     // for(let i = 1400; i < 1600; i++){
+//     // for(let i = 1600; i < 1800; i++){
+//     // for(let i = 1800; i < 2000; i++){
+//     // for(let i = 2000; i < 2200; i++){
+//         request('https://launchlibrary.net/1.4/launch/' + i, (err, res) =>{
+//             if(!err && res.statusCode === 200){
+//                 let responseObject = JSON.parse(res.body);
+//                 launchesArray = responseObject.launches;
+//                 launchesArray.forEach(launch => {
+//                     Launch.findOne({id: launch.id, changed: launch.changed}, (error, document) =>{
+//                         if(error){
+//                             console.log(error);
+//                         }
+//                         if(document){
+//                             // console.log("launches already in database");
+//                         }
+//                         else{
+//                             console.log("new document", launch.id);
+//                             Location.findOne({ id: launch.location.id }, (err, foundLocation) => {
+//                                 // console.log('found location');
+//                             })
+//                             .then((foundLocation) => {
+//                                 launch.location = foundLocation;
+//                                 Agency.findOne({ id: launch.lsp.id }, (err, foundAgency) => {
+//                                     // console.log('found agency');
+//                                 })
+//                                 .then((foundAgency) => {
+//                                     launch.lsp = foundAgency;
+//                                     Rocket.findOne({ id: launch.rocket.id }, (err, foundRocket) => {
+//                                         // console.log('found rocket');
+//                                     })
+//                                     .then((foundRocket) => {
+//                                         launch.rocket = foundRocket;
+//                                         Launch.create(launch, (err) => {
+//                                             if(err) {
+//                                                 console.log(err);
+//                                             }
+//                                             else {
+//                                                 console.log(`Launch ${launch.id} saved to database`);
+//                                             }
+//                                         });
+//                                     })
+//                                 })
+//                             })
+//                         }
+//                     });
+//                 })
+//             }
+//         });
+//     }
+// });
 
 // Query database to get all launches then send results to frontend
 router.get('/', (req, res) =>{
@@ -83,11 +83,11 @@ router.get('/', (req, res) =>{
     });
 });
 
-cron.schedule('22 * 10 * * 1', () =>{
-// cron.schedule('8 * * * * *', () =>{
-    // updateLocations();
-    updateRockets();
-});
+// cron.schedule('22 * 10 * * 1', () =>{
+// // cron.schedule('8 * * * * *', () =>{
+//     // updateLocations();
+//     updateRockets();
+// });
 
 updateLocations = () =>{
     Launch.find({}, (err, res) =>{
@@ -165,21 +165,5 @@ router.get('/:launchId', (req, res) =>{
         }
     });
 });
-
-testSearch = () =>{
-    Launch.find({
-        $text:{
-            $search: 'soyuz',
-            $caseSensitive: false,
-        }
-    }, (err, res) =>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            console.log(res);
-        }
-    });
-}
 
 module.exports = router;
