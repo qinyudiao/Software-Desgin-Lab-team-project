83d73436c09c03941c7db1044351333ee7184032
lbest42 <lucasbest@utexas.edu>
2020-04-23 17:36:21 -0500

    Frontend unit tests

---
+++ frontend/src/__tests__/frontend_tests.js
@@ -0,0 +1,105 @@
+import React from 'react';
+import Agencies from '../Agencies.js';
+import About from '../About.js';
+import App from '../App.js';
+import Astronauts from '../Astronauts.js';
+import Education from '../Education.js';
+import InternationalAstronauts from '../InternationalAstronauts.js';
+import Landing from '../Landing.js';
+import Lauches from '../Launches.js';
+import Map from '../Map.js';
+import PageNotFound from '../PageNotFound.js';
+import RussianAstronauts from '../RussianAstronauts.js';
+import UpcomingLaunches from '../UpcomingLaunches.js';
+import ReactDOM from 'react-dom';
+
+describe('Agencies', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Agencies/>, div);
+    });
+});
+
+describe('About', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<About/>, div);
+    });
+});
+
+describe('App', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<App/>, div);
+    });
+});
+
+describe('Astronauts', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Astronauts/>, div);
+    });
+});
+
+describe('Education', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Education/>, div);
+    });
+});
+
+describe('InternationalAstronauts', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<InternationalAstronauts/>, div);
+    });
+});
+
+describe('Landing', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Landing/>, div);
+    });
+});
+
+describe('Lauches', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Lauches/>, div);
+    });
+});
+
+describe('Map', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Map/>, div);
+    });
+});
+
+describe('PageNotFound', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<PageNotFound/>, div);
+    });
+});
+
+describe('Map', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<Map/>, div);
+    });
+});
+
+describe('RussianAstronauts', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<RussianAstronauts/>, div);
+    });
+});
+
+describe('UpcomingLaunches', () => {
+    it('renders without crashing', () => {
+        const div = document.createElement('div');
+        ReactDOM.render(<UpcomingLaunches/>, div);
+    });
+});
