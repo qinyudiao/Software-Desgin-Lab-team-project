7ed0e198686ff32bcbdd197bd8ad0f74321e279b
Musa Rafik <musa_rafik@utexas.edu>
2020-04-28 21:37:13 -0500

    .

--- backend/app.js
+++ backend/app.js
@@ -233,5 +233,5 @@ app.post('/search', async (req, res) =>{
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(app);
 
-httpServer.listen(8080);
-httpsServer.listen(8443);
+httpServer.listen(80);
+httpsServer.listen(443);
