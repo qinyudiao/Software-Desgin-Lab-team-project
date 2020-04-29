7b141be27996a3fbbe32c21cc4322214fd1a795e
Musa Rafik <musa_rafik@utexas.edu>
2020-04-28 21:24:24 -0500

    working on https

--- backend/app.js
+++ backend/app.js
@@ -228,7 +228,7 @@ app.post('/search', async (req, res) =>{
 });
 
 const port = process.env.PORT || 8080;
-// app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 var httpServer = http.createServer(app);
 var httpsServer = https.createServer(app);
